0fb9c7ed4024eb8c2e3d95e80fbb34f12e05d3aa
Leonardo-bit999 <leosantos.silva9925@gmail.com>
2026-09-19 12:59:09 -0300

    Navbar e Hero atualizados

--- src/app/features/home/sections/navbar/navbar.ts
+++ src/app/features/home/sections/navbar/navbar.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  protected readonly menuOpen = signal(false);
 
+  protected toggleMenu(): void {
+    this.menuOpen.update((isOpen) => !isOpen);
+  }
+
+  protected closeMenu(): void {
+    this.menuOpen.set(false);
+  }
 }
